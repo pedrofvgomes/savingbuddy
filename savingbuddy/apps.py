ff0f50420aa80859a1285d3f4a6ece4d3ccfcfc1
Pedro Gomes <up202108825@up.pt>
2023-08-20 09:07:46 +0100

    authentication template done, still working on the login/signup logic

---
+++ savingbuddy/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SavingbuddyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'savingbuddy'
---
+++ savingbuddy/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('authentication', views.authentication, name='authentication')
+]
