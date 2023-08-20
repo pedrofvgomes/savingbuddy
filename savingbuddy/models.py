022ce02b118c7c1738600504277d5f0097140d11
Pedro Gomes <up202108825@up.pt>
2023-08-20 13:54:10 +0100

    removed authentication.html, views and urls, gonna try a new approach

--- savingbuddy/models.py
+++ savingbuddy/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
-    pass
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    activesince = models.DateTimeField(auto_now_add=True)
+    balance = models.IntegerField(default=0)
--- savingbuddy/views.py
+++ savingbuddy/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+import bcrypt
+
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
@@ -7,50 +9,3 @@ from django.db import IntegrityError
 from .models import User
 
 # Create your views here.
-def authentication(request):
-    return render(request, "savingbuddy/authentication.html")
-
-def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse('index'))
-        else:
-            return render(request, "savingbuddy/authentication.html",{
-                "message": "Invalid username and/or password."
-            })
-    else:
-        return render(request, "savingbuddy/authentication.html")
-    
-def logout(request):
-    logout(request)
-    return HttpResponseRedirect(reverse('index'))
-
-def signup(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-
-        password = request.POST['password']
-        password2 = request.POST['password2']
-        if password != password2:
-            return render(request, "savingbuddy/authentication.html",{
-                "message" : "Passwords don't match."
-            })
-        
-        try:
-            user = User.objects.create_user(username, email, password)
-            user.save()
-        except IntegrityError:
-            return render(request, "savingbuddy/authentication.html",{
-                "message" : "Username already taken."
-            })
-        
-        login(request, user)
-        return HttpResponseRedirect(reverse('index'))
-    else:
-        return HttpResponseRedirect('savingbuddy/authentication.html')
